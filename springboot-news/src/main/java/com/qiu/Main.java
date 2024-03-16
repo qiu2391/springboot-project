28b0d64fc2b9e9ce7f7f11de883110e9c91fa4c6
qiuzhifeng <3089476052@qq.com>
2024-03-16 15:21:48 +0800

    hot-fix 1

--- springboot-news/src/main/java/com/qiu/Main.java
+++ springboot-news/src/main/java/com/qiu/Main.java
@@ -16,6 +16,7 @@ import org.springframework.context.annotation.Bean;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Hello world!hot-fix");
         SpringApplication.run(Main.class,args);
     }
     @Bean
